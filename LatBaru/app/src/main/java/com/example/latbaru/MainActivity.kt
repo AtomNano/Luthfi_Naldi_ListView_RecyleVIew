04e4bdb8b0099a639156377b168d4ffdeb281fb5
Muhammad Luthfi Naldi <129099749+AtomNano@users.noreply.github.com>
2024-09-11 23:53:56 +0700

    LatihanKotlin

---
+++ LatBaru/app/src/main/java/com/example/latbaru/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.latbaru
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var eUser : EditText
+    private lateinit var ePassword : EditText
+    private lateinit var btnLogin : Button
+    private lateinit var btnList : Button
+    private lateinit var btnRecycle : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        eUser = findViewById(R.id.eUser)
+        ePassword = findViewById(R.id.ePassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnList = findViewById(R.id.btnList)
+        btnRecycle = findViewById(R.id.btnRecycle)
+        btnLogin.setOnClickListener {
+            val nUsername = eUser.text.toString()
+            val nPassword = ePassword.text.toString()
+
+            val intent = Intent(this, WelcomePageActivity::class.java)
+            intent.putExtra("username", nUsername)
+            intent.putExtra("password", nPassword)
+
+            Log.d("cek data username", nUsername)
+            Log.d("cek data Password", nPassword)
+
+            startActivity(intent)
+        }
+
+        btnList.setOnClickListener {
+            val intent = Intent(this, ListViewActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRecycle.setOnClickListener {
+            val intent = Intent(this, RecycleViewActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
